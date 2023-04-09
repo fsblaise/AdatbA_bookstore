d1b639ed31a2208cfb87b4f5c397ef66ae44b9fc
Gergo Csizi <gergocs@inf.u-szeged.hu>
2023-04-09 09:08:34 +0200

    Add some new functionality and fix some issues

--- bookStore/src/main/java/com/bookstore/bookstore/controllers/MainController.java
+++ bookStore/src/main/java/com/bookstore/bookstore/controllers/MainController.java
@@ -20,9 +20,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -50,7 +48,7 @@ public class MainController {
     ArrayList<Product> data;
 
     @FXML
-    public void initialize() throws NoSuchFieldException {
+    public void initialize() {
         onProductsButtonClick();
     }
 
@@ -126,7 +124,7 @@ public class MainController {
     }
 
     @FXML
-    public void onProductsButtonClick() throws NoSuchFieldException {
+    public void onProductsButtonClick() {
         data = DAO.instance().runCustomQuery(Product.class, "SELECT * FROM BOOK_STORE_PRODUCT ORDER BY production DESC, review DESC");
         List<Object[]> types = DAO.instance().listTypes();
 
@@ -175,15 +173,15 @@ public class MainController {
         storeButtons.clear();
         right.getChildren().clear();
 
-        List<Object[]> stores = DAO.instance().listStores();
-        System.out.println(stores);
-        for(Object[] store : stores)
+        List<Object> stores = DAO.instance().listStores();
+
+        for(Object store : stores)
         {
-            Button r = new Button(store[0] + " : " +store[1]);
+            Button r = new Button(store.toString());
             r.setMinWidth(220);
             r.setTextAlignment(TextAlignment.CENTER);
             r.setOnAction(event -> {
-                data = DAO.instance().getAllByStore(store[0].toString());
+                data = DAO.instance().getAllByStore(store.toString());
                 generateList(data);
             });
             storeButtons.add(r);
